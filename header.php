848974eda086f8f9ca1b720b90977514f1c8401b
apathchy1 <Apathchy@gmail.com>
2018-09-10 21:35:11 +0300

    comit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+<title>Пътеводител и Каталог : НИАР - Плиска</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width = device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="styles/style.css" type="text/css"/>
+
+<div style="position:relative">
+<img src="src/logo.png" class="roseta"/>
+</div>
+    </head>
+<body>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include 'header.php';
+?>
+<body bgcolor="#57363E">
